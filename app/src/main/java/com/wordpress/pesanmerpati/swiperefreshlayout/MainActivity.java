342c0dac9d1dd233e1f007b01c211458cf9d9b19
pesanmerpati <rohmatsuseno@gmail.com>
2018-01-20 21:41:37 +0700

    update commit

--- app/src/main/java/com/wordpress/pesanmerpati/swiperefreshlayout/MainActivity.java
+++ app/src/main/java/com/wordpress/pesanmerpati/swiperefreshlayout/MainActivity.java
@@ -1,13 +1,41 @@
 package com.wordpress.pesanmerpati.swiperefreshlayout;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView mListView;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = findViewById(R.id.listView);
+        String[] android_versions = getResources().getStringArray(R.array.android_versions);
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android_versions);
+        mListView.setAdapter(mAdapter);
+
+        mSwipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.holo_green_light,
+                R.color.holo_blue_bright,
+                R.color.holo_orange_light,
+                R.color.holo_red_light);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(MainActivity.this, "No data entry!", Toast.LENGTH_SHORT).show();
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
 }
